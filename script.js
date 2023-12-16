544f294a92473e4a7b2854f243f960192564f86b
3123MtOlympus <realchrislondon@gmail.com>
2023-12-16 13:23:27 -0800

    start code

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  var specialChar = ["!", "@", "#", "$", "&", "*", "_", "?"]
+  var lowerCasedChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var numberOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var upperCasedChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  
+  
+let specialChar = prompt("Do you want to use Special Character's?");
+switch(specialChar) {
+  case "yes":
+}
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
