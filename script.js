c1cf45946aadb1a91d8c57d775bd3c3b6358d427
3123MtOlympus <realchrislondon@gmail.com>
2023-12-18 17:01:45 -0800

    commit test

--- script.js
+++ script.js
@@ -9,15 +9,28 @@ function writePassword() {
   var lowerCasedChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var numberOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var upperCasedChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  
-  
-let specialChar = prompt("Do you want to use Special Character's?");
-switch(specialChar) {
-  case "yes":
-}
+
   passwordText.value = password;
 
+}
+function getPassword() {
+  var length = parseInt( prompt("Password generated will be between 8 and 128 character."),);
+  var hasSpecialChar = confirm(
+    'Click OK to confirm');
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+let writePassword = prompt("Do you want to use Special Character's?");
+switch(specialChar) {
+  case "yes":
+    text = "Sure thing!";
+    break;
+    case "no":
+      text = "No problem";
+
+
+}
